b153dc22871e0c9678aef859f0079ddbf028a396
madspecco <bogdan.bolos30@gmail.com>
2022-08-30 22:24:26 +0300

    Started Creating Updating Slider

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // create divs using js
-
 const squareContainer = document.querySelector('#squareContainer');
-// const squares = document.querySelectorAll('.squares');
+const sizeSlider = document.getElementById('#slider');
 
+
+// use the default value for the grid by calling a function with innerHTML
+const DEFAULT_SIZE = 16;
+
+// setting defautl size;
+let currentSize = DEFAULT_SIZE;
+
+function setSize(size) {
+    currentSize = size;
+}
+
+
+console.log(currentSize);
 
 for(let  i = 0; i < 32*32; i++) {
     // create div
