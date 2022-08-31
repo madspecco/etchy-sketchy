960d9889351d6c3b9307d25cfb8e1b3e22917a04
madspecco <bogdan.bolos30@gmail.com>
2022-08-31 11:40:38 +0300

    Made Slider change it's value on input

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 // create divs using js
 const squareContainer = document.querySelector('#squareContainer');
-const sizeSlider = document.getElementById('#slider');
-
+const slider = document.querySelector('#slider');
+const sizeValue = document.querySelector('#sliderSize');
 
 // use the default value for the grid by calling a function with innerHTML
 const DEFAULT_SIZE = 16;
 
-// setting defautl size;
 let currentSize = DEFAULT_SIZE;
 
-function setSize(size) {
-    currentSize = size;
+// get slider value and change slider value
+
+slider.oninput = function () {
+
+    sizeValue.innerHTML = `${this.value} x ${this.value}`;
+    console.log(this.value);
 }
 
+// now onto changing the grid according to the slider
 
-console.log(currentSize);
 
 for(let  i = 0; i < 32*32; i++) {
     // create div
